c5e6e81711cd15aaf0f20d1f4391d24a05caddb9
jeremy <j@e.com>
2022-02-22 15:43:01 -0800

    Cleaned up and swapped to pexpect to run commands

--- icmx-api/src/datachecker.py
+++ icmx-api/src/datachecker.py
@@ -1,53 +1,96 @@
-import json, re
+import json, re, pexpect
 
-def checkResult(raw_output):
-    #Data categories = client_license, client_list, group_list, host_list, error]
-    result_json = {'return_code': 0, 'data': '', 'data_category':'' }
-    if (raw_output.__contains__('Client License:')):
-        sanitized = raw_output.strip("Client License:").replace("\n", "")
-        result_json['data'] = json.loads(sanitized)
-        result_json['data_category'] = 'client_license'
-    elif (raw_output.__contains__('GDPXL License:')):
-        sanitized = raw_output.strip("GDPXL License:").replace("\n", "")
-        result_json['data'] = json.loads(sanitized)
-        result_json['data_category'] = 'client_license'
-    elif(raw_output.__contains__('Customer (\"q\" to quit): None found')):
-        client_list = reduceStr(raw_output)
-        client_list.remove('Customer (\"q\" to quit): None found')
-        result_json['data'] = client_list
-        result_json['data_category'] = 'client_list'
-    elif(raw_output.__contains__('Server Group (\"q\" to quit): None found')):
-        group_list = reduceStr(raw_output)
-        group_list.remove('Server Group (\"q\" to quit): None found')
-        result_json['data'] = group_list
-        result_json['data_category'] = 'group_list'
-    elif(raw_output.__contains__('Server Hostname (\"q\" to quit): None found')):
-        host_list = reduceStr(raw_output)
-        host_list.remove('Server Hostname (\"q\" to quit): None found')
-        result_json['data'] = host_list
-        result_json['data_category'] = 'host_list'
-    elif(raw_output.__contains__('Would you like to send these licenses to the customer?')):
-        result_json['data'] = raw_output
-        result_json['data_category'] = 'notify'
-    elif (raw_output == ""):
-        result_json['data'] = raw_output
-        result_json['data_category'] = 'blank'
-    else:
-        result_json['data'] = raw_output
-        result_json['data_category'] = 'error'
-        result_json['return_code'] = 1
-    return result_json
+def runExpect(cmdstr,**kwargs):
+    exp_list = [
+        pexpect.EOF, #0
+        'Client License:',
+        'GDPXL License:',
+        'Customer \(\"q\" to quit\)\:',
+        'Server Group \(\"q\" to quit\)\:', #4
+        'Server Hostname \(\"q\" to quit\)\:',
+        'Would you like to send these licenses to the customer\?',
+        '\w+-\w+-\w+-\w+-\w+',
+        'Customer .* already has server groups. +Do you mean to create another\? +\[y\/N\]',  # 8
+        'None found',
+        'Customer .* does not yet exist. +Create it\? +\[y\/N\]\:',
+        'This server group is now empty\. +Delete the group too\?',
+        'Would you like to delete group .*\?', #12
+        'Would you like to delete customer .*\?',
+        '.*=.*'
+    ]
+    result_json = {'return_code': 0, 'data': '', 'data_category':'', 'cp_exit_code':0}
 
-def checkPropVal(raw_output,cmdstr):
-    result_json = {'return_code': 0, 'data': raw_output, 'data_category': 'propval'}
-    if (raw_output.__contains__("Couldn't find")):
+    try:
+        ch_process = pexpect.spawnu(cmdstr)
+        while 1:
+            index = ch_process.expect(exp_list)
+            if index == 1 or index == 2:
+                replacers = {'\r\n':'','\n':'','\b':''}
+                ch_process.expect("\{.*\}")
+                sanitized = ch_process.after
+                for pattern,newval in replacers.items():
+                    sanitized = sanitized.replace(pattern,newval)
+                result_json['data'] = json.loads(sanitized)
+                result_json['data_category'] = 'client_license'
+                ch_process.sendline("q")
+                break
+            elif index == 3:
+                client_list = reduceStr(ch_process.before)
+                result_json['data'] = client_list
+                result_json['data_category'] = 'client_list'
+                ch_process.sendline("q")
+            elif index == 4:
+                group_list = reduceStr(ch_process.before)
+                result_json['data'] = group_list
+                result_json['data_category'] = 'group_list'
+                ch_process.sendline("q")
+            elif index == 5:
+                host_list = reduceStr(ch_process.before)
+                result_json['data'] = host_list
+                result_json['data_category'] = 'host_list'
+                ch_process.sendline("q")
+            elif index == 6:
+                result_json['data'] = ch_process.before
+                if result_json['data_category'] == '':
+                    result_json['data_category'] = 'notify'
+                if kwargs['notify']:
+                    ch_process.sendline("y")
+                else:
+                    ch_process.sendline("n")
+            elif index == 7:
+                result_json['data'] = ch_process.after
+                result_json['data_category'] = 'uuid'
+                break
+            #elif index == 8 or index == 10 or index == 11 or index == 12 or index == 13:
+            elif index in [8,10,11,12,13]:
+                result_json['data_category'] = 'obj_manip'
+                ch_process.sendline("y")
+            elif index == 9:
+                break
+            elif index == 14:
+                result_json['data'] = re.sub('\r\n', '', ch_process.after)
+                result_json['data_category'] = 'quotas'
+                break
+            elif index == 0: #A catch-all for tricky patterns w/o prompts
+                result_json['data'] = ch_process.before
+                result_json['data_category'] = 'noprompt'
+                break
+            else:
+                result_json['data'] = "Expect index out of bounds."
+                result_json['data_category'] = 'error'
+                result_json['return_code'] = 1
+                break
+        ch_process.close()
+        if ch_process.exitstatus == None:
+            result_json['cp_exit_code'] = 0
+        else:
+            result_json['cp_exit_code'] = ch_process.exitstatus
+        return result_json
+    except Exception as e:
+        result_json['data'] = str(e)
+        result_json['data_category'] = 'exception'
         result_json['return_code'] = 1
-        result_json['data_category'] = 'error'
-    if (cmdstr.__contains__("Uuid")):
-        result_json['data_category'] = 'Uuid'
-    if (cmdstr.__contains__("Quotas")):
-        result_json['data_category'] = 'Quotas'
-    return result_json
+        return result_json
 
 def reduceStr(raw_str):
     trimmed_str = re.sub('\d+ +','',raw_str)
--- icmx-api/src/icmapi.py
+++ icmx-api/src/icmapi.py
@@ -1,8 +1,8 @@
-import subprocess, os
+import subprocess, os, pexpect
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
-from datachecker import checkResult, checkPropVal
+from datachecker import runExpect
 app = Flask(__name__)
 
 #Add module that prevents cross-origin requests from getting blocked in javascript
@@ -13,58 +13,52 @@ api = Api(app)
 script_dir = "/home/jeremy/Documents/p4_stuff/license_scripts"
 os.chdir(script_dir)
 
-input_prefix = "echo 'q' |" #bypass input prompts during icmx cmds
-nonotify_prefix = "echo 'N' |" #Avoid sending email after renew command
-notify_prefix = "echo 'y' |" #Avoid sending email after renew command
-add_prefix = "echo -e 'y\nn' |" #Adding customer, avoid sending notice
-
 class Search(Resource):
     def get(self, script_name):
         param_str = argsToFlags(request.args)
-        script_str = "%s ./%s search %s" % (input_prefix, script_name, param_str)
+        script_str = "./%s search %s" % (script_name, param_str)
         return runCmd(script_str)
 
 class Renew(Resource):
     def post(self, script_name):
         param_str = argsToFlags(request.args)
         param_str = dataToFlags(request.json, param_str)
-        script_str = "%s ./%s renew %s" % (nonotify_prefix, script_name, param_str)
-        return runCmd(script_str)
+        script_str = "./%s renew %s" % (script_name, param_str)
+        return runCmd(script_str, notify=False)
 
 class Notify(Resource):
     def post(self, script_name):
         param_str = argsToFlags(request.args)
-        script_str = "%s ./%s notify %s" % (notify_prefix, script_name, param_str)
-        return runCmd(script_str)
+        script_str = "./%s notify %s" % (script_name, param_str)
+        return runCmd(script_str, notify=True)
 
 class Propval(Resource):
     def __init__(self):
         self.script_name = "icmgdpxlt"
 
     def get(self):
-        #Gets all propvals with multiple cmds, combine results.
         param_str = argsToFlags(request.args)
         script_str = "./%s propval %s" % (self.script_name, param_str)
-        return runCmd(script_str,prop_check=True)
+        return runCmd(script_str)
 
     def post(self):
         param_str = argsToFlags(request.args)
         param_str = dataToFlags(request.json, param_str)
-        script_str = "./icmgdpxlt propval %s" % (param_str)
-        return runCmd(script_str,prop_check=True)
+        script_str = "./%s propval %s" % (self.script_name,param_str)
+        return runCmd(script_str)
 
 class Clone(Resource):
     def post(self, script_name):
         param_str = argsToFlags(request.args)
-        script_str = "%s ./%s clone %s" % (nonotify_prefix, script_name, param_str)
-        return runCmd(script_str)
+        script_str = "./%s clone %s" % (script_name, param_str)
+        return runCmd(script_str, notify=False)
 
 class Add(Resource):
     def post(self, script_name):
         param_str = argsToFlags(request.args)
         param_str = dataToFlags(request.json, param_str)
-        script_str = "%s ./%s add %s" % (add_prefix, script_name, param_str)
-        return runCmd(script_str)
+        script_str = "./%s add %s" % (script_name, param_str)
+        return runCmd(script_str, notify=False)
 
 class Delete(Resource):
     def post(self, script_name):
@@ -72,30 +66,22 @@ class Delete(Resource):
         script_str = "./%s delete %s" % (script_name, param_str)
         return runCmd(script_str)
 
-def runCmd(cmdstr, prop_check=False):
-    process = subprocess.Popen(cmdstr, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    if prop_check:
-        if not process.returncode:
-            result_json = checkPropVal(stdout.decode('UTF-8'),cmdstr)
-        else:
-            result_json = {}
-            result_json['return_code'] = process.returncode
-            result_json['data'] = stdout.decode('UTF-8')
-            result_json['data_category'] = 'error'
-    else:
-        result_json = checkResult(stdout.decode('UTF-8'))
-    return fmtOutput(cmd=cmdstr,stdout=result_json,stderr=stderr.decode('UTF-8'),proc_code=process.returncode)
+def runCmd(cmdstr, **kwargs):
+    result_json = runExpect(cmdstr,**kwargs)
+    return fmtOutput(cmd=cmdstr, result=result_json)
 
-def fmtOutput(cmd="",stdout="",stderr="",proc_code=0):
-    response = jsonify({"cmd":cmd, "results":stdout, "stderr":stderr,"proc_code":0})
+def fmtOutput(cmd="", result=None):
+    response = jsonify({"cmd":cmd, "results":result})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 def argsToFlags(req_args):
     param_str = ""
     if "customer" in req_args.keys():
-        param_str = "-c ^%s$" % req_args['customer']
+        if "noregex" in req_args.keys():
+            param_str = "-c %s" % req_args['customer']
+        else:
+            param_str = "-c ^%s$" % req_args['customer']
     if "group" in req_args.keys():
         param_str = param_str + " -g %s" % req_args['group']
     if "host" in req_args.keys():
@@ -110,7 +96,7 @@ def dataToFlags(data, param_str):
     if "users" in data.keys():
         param_str = param_str + " -u %s" % data['users']
     if "property_value" in data.keys():
-        param_str = param_str + " -l %s" % data['property_value']
+        param_str = param_str + " -l \'%s\'" % data['property_value']
     if "administrators" in data.keys():
         param_str = param_str + " -a %s" % data['administrators']
     return param_str
